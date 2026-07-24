6df1336866f831949a8bded22a75c4b3138ff62f
Vijay Mathew Pandyalakal <mathew.vijay@gmail.com>
2026-07-24 16:01:32 +0530

    release 0.3.1 (#147)

--- editor/emacs/nex-mode.el
+++ editor/emacs/nex-mode.el
@@ -4,7 +4,7 @@
 
 ;; Author: Nex Contributors
 ;; Keywords: languages, nex, design-by-contract
-;; Version: 0.3.0
+;; Version: 0.3.1
 ;; Package-Requires: ((emacs "24.3"))
 ;; URL: https://github.com/vijaymathew/nex
 
