9fbdf3169278531960be904ca954e343f2d25e6a
Vijay Mathew <mathew.vijay@gmail.com>
2026-08-14 21:06:29 +0530

    v0.3.5 release

--- src/nex/repl.clj
+++ src/nex/repl.clj
@@ -1826,7 +1826,7 @@
 
 (defn show-banner []
   (println "╔════════════════════════════════════════════════════════════╗")
-  (println "║                   NEX REPL v0.3.4                          ║")
+  (println "║                   NEX REPL v0.3.5                          ║")
   (println "║     A high-level language for design and implementation    ║")
   (println "╚════════════════════════════════════════════════════════════╝")
   (println)
